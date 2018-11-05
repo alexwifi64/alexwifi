736785d0e1be962288fa8e32e9375d9bc11b409a
alexwifi64 <39088772+alexwifi64@users.noreply.github.com>
2018-11-05 09:27:47 +0100

    Update exercici algoritmes/la vida algoritmos de uso.cpp

---
+++ exercici algoritmes/la vida algoritmos de uso.cpp
@@ -0,0 +1,201 @@
+10/10/2018
+top #1
+
+Taki Taki (with Selena Gomez, Ozuna & Cardi B)
+– DJ Snake, Selena Gomez, Ozuna, Cardi B 
+
+Prende los motores Kawasaki
+Que la disco está llena
+
+if (disco está llena){
+prende los motores kawasaki;
+}
+
+No trajo pantisito pa' que el nene no trabaje
+
+if (el nene no quiere trabajar){
+no traer pantisito;
+}
+
+He said he wants to touch it, and tease it, and squeeze it
+Well my piggy bank is hungry, my nigga, you need to feed it
+
+if (i want to touch it || tease it || squeeze it){
+feed her piggy bank;
+}
+
+I'm a whole rich bitch, and I work like I'm broke still
+
+if (i’m a rich bitch){
+work like i’m broke;
+}
+
+
+
+top #2
+
+BEBE – 6ix9ine, Anuel Aa 
+
+
+top #3
+
+Vaina Loca – Ozuna, Manuel Turizo 
+
+Con nadie se compromete, y menos si tú le promete
+
+if (prometo){
+ella no se compromete;
+}
+
+Porque cuando te tengo cerquita
+Me entra una vaina loca que después no se me quita
+
+if (te tengo cerquita){
+me entra una vaina loca;
+}
+
+top #4
+
+TELÉFONO – Aitana 
+
+Hoy he dejado mi teléfono para no llamarte
+
+if (dejo mi telefono){
+no te llamo;
+}
+
+Para no llamarte, para así olvidarte
+
+if (no te llamo){
+te olvido;
+}
+
+
+top #5
+
+Quiere Beber – Anuel Aa 
+
+Ella se entregó y el tipo le falló
+Y por eso se va a rumbear (rumbear)
+
+Ella quiere beber, ella quiere bailar (bailar)
+
+if (el tipo le falla){
+se va a rumbear;
+ella quiere beber;
+ella quiere bailar;
+}
+
+Ella se emborrachó-ó y conmigo se escapó-ó-ó
+
+if (ella se emborracha){
+se escapa conmigo;
+}
+
+top #6
+
+Mujeres – Mozart La Para, Justin Quiles 
+
+Beber, joder, hasta que no pueda más
+Que la noche sigue
+
+if (la noche sigue){
+beber, joder hasta que no pueda más;
+}
+
+¿Estás nerviosa? Mastica hielo, café... tranquila
+
+if (estás nerviosa){
+mastica hielo;
+toma café;
+}
+
+Y cuando se ennota, conmigo lo goza (jejeje)
+Se pone sabrosa y me entrega esa cosa (dame la mitad manin)
+
+if (ella se ennota){
+se pone sabrosa;
+me entrega esa cosa;
+}
+
+top #7
+
+Celoso – Lele Pons 
+
+Yo no tengo tiempo pa' tu' drama'
+A mí no me llame' a la cinco 'e la mañana
+
+if (me llamas at 5am){
+no tengo tiempo pa tu drama;
+}
+
+Te pone' celoso si me ve' con otro
+
+if (me ves con otro){
+te pones celoso;
+}
+Sé que me celas y yo te veo
+Y tú me miras, yo me meneo (ey)
+
+if (tu te celas){
+yo me meneo;
+}
+
+Yo no pregunto si lo quiero, yo lo hago
+
+if (quiero algo){
+no pregunto;
+lo hago;
+}
+
+
+top #8
+
+No Es Justo – J Balvin, Zion & Lennox 
+
+Si te acercas a mí, no pares
+
+if (te acercas){
+no pares;
+}
+
+Y si te digo "Está linda" una y otra vez
+Eso te gusta y lo sabes (heh, heh, heh)
+
+if (te digo estás linda){
+te gusta;
+}
+
+top #9
+
+No Me Acuerdo – Thalía, Natti Natasha 
+
+Y que te monté los cuernos
+De eso no me acuerdo, no pasó, no pasó
+
+if (te monté los cuernos){
+no me acuerdo;
+no pasó;
+}
+
+Tú no tiene' nada que decir, cara dura
+Dijiste a las diez y llegaste a la una
+La otra vez, y me quedé esperándote
+Yo también tengo derecho a pasarla bien
+
+if (tu llegas a la una){
+yo también la puedo pasar bien;
+}
+
+
+top #10
+
+Cuando Te Besé – Becky G, Paulo Londra 
+
+No sé por qué todo el tiempo yo quiero besarte
+Quizá', tal vez, nací para amarte (nací para amarte)
+
+if (quiero besarte todo el tiempo){
+nací para amarte;
+}
+
