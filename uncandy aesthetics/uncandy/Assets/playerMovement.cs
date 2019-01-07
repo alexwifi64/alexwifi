57af828ae8177939f8cdff1a2023c986c8a10f2d
alexwifi64 <39088772+alexwifi64@users.noreply.github.com>
2019-01-07 18:30:19 +0100

    Create playerMovement.cs

---
+++ uncandy aesthetics/uncandy/Assets/playerMovement.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    //velocitat del player quan es mou amb wasd
+    float speed = 0.5f;
+    
+    // aquesta només s'executa al principi
+    void Start(){
+
+        Debug.Log("hello world");
+        
+    }
+
+    // aquesta es fa executant a cada frame
+    void Update()
+    {
+        //si es piquen tecles wasd mou el player
+
+        if (Input.GetKey("w"))
+        {
+            Vector3 position = this.transform.position;
+            position.x = position.x + speed;
+            this.transform.position = position;
+        }
+
+        if (Input.GetKey("a"))
+        {
+            Vector3 position = this.transform.position;
+            position.z = position.z + speed;
+            this.transform.position = position;
+        }
+
+        if (Input.GetKey("s"))
+        {
+            Vector3 position = this.transform.position;
+            position.x = position.x - speed;
+            this.transform.position = position;
+        }
+
+        if (Input.GetKey("d"))
+        {
+            Vector3 position = this.transform.position;
+            position.z = position.z - speed;
+            this.transform.position = position;
+        }
+
+
+    }
+}
